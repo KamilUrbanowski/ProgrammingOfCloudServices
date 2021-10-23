278a37624dbe632383671b187deab306a6e25df9
Kamil Mandas <kamil.mandas@gmail.com>
2021-10-23 18:39:08 +0200

    Initial commit

---
+++ client/src/MainComponent.js
@@ -0,0 +1,57 @@
+import { useCallback, useState, useEffect } from "react";
+import axios from "axios";
+import "./MainComponent.css";
+
+const MainComponent = () => {
+  const [values, setMessages] = useState([]);
+  const [value, setValue] = useState("");
+
+  const getAllMessages = useCallback(async () => {
+    // we will use nginx to redirect it to the proper URL
+    const data = await axios.get("/api/messages/all");
+    setMessages(data.data.rows.map(row => row.data));
+    
+  }, []);
+
+  const saveMessage = useCallback(
+    async event => {
+      event.preventDefault();
+
+      await axios.post("/api/messages", {
+        value
+      });
+
+      setValue("");
+    },
+    [value]
+  );
+
+  useEffect(() => {
+    getAllMessages();
+  }, []);
+
+  return (
+    <div>
+      <button onClick={getAllMessages}>Get all messages</button>
+      <br />
+      <span className="title">Messages</span>
+      <div className="messages">
+        {values.map(value => (
+          <div className="message">{value}</div>
+        ))}
+      </div>
+      <form className="form" onSubmit={saveMessage}>
+        <label>Enter your message: </label>
+        <input
+          value={value}
+          onChange={event => {
+            setValue(event.target.value);
+          }}
+        />
+        <button>Submit</button>
+      </form>
+    </div>
+  );
+};
+
+export default MainComponent;
---
+++ server/index.js
@@ -0,0 +1,46 @@
+const keys = require("./keys");
+
+const express = require("express");
+const bodyParser = require("body-parser");
+const cors = require("cors");
+
+const app = express();
+app.use(cors());
+app.use(bodyParser.json());
+
+
+const { Pool } = require("pg");
+const pgClient = new Pool({
+    user: keys.pgUser,
+    host: keys.pgHost,
+    database: keys.pgDatabase,
+    password: keys.pgPassword,
+    port: keys.pgPort
+});
+
+pgClient.on("connect", (client) => {
+    client.query("CREATE TABLE IF NOT EXISTS message (data TEXT)")
+    .catch(err => console.log("PG ERROR", err));
+});
+
+app.get("/", (req, res) => {
+    res.send("Hello");
+});
+
+app.get("/messages/all", async (req, res) => {
+    const values = await pgClient.query("SELECT * FROM message");
+
+    res.send(values);
+});
+
+app.post("/messages", async (req, res) => {
+    if(!req.body.value) res.send({ working: false});
+
+    pgClient.query("INSERT INTO message(data) VALUES($1)", [req.body.value]);
+
+    res.send({ working: true});
+});
+
+app.listen(5000, err => {
+    console.log("Listening");
+})
